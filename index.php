4241e2d654df0ca7d434241e22b614e48257aff9
Justin Hines <jhines@marketingadept.com>
2013-10-23 17:52:02 -0400

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$root = "http://localhost/m3/html";
+	$root = "http://localhost/m3/site";
 ?>
 <!DOCTYPE html>
 <head>
@@ -21,13 +21,24 @@
 	            <div class="scroller-inner">
 						<a href="#" id="trigger" class="menu-trigger">Open/Close Menu</a>
 						
-						<div class="page-content">
-							<h1>Page Title</h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<div class="page-content cover">
+							<br><br><br><br><br>
+							<small>[image here]</small>
+							<br><br><br><br><br>
+							<h1>Making a Molehill out of a Mountain</h1>
+							<h2>Optimization Techniques for Responsively-Designed Websites</h2>
+
+							
+							
 						</div><!-- .page-content -->
 						
-	                </div>
+						
 	            </div><!-- /scroller-inner -->
+				<div class="pagination">
+					<a href="" class="prev">Previous</a>
+					<a href="" class="next">Next</a>
+				</div><!-- .pagination -->
+	
 	        </div><!-- /scroller -->
 
 	    </div><!-- /pusher -->
