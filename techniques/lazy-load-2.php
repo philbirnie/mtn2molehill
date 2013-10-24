2d8089b735f1909f4c0cd757b0a35197cbcbabbc
Phil Birnie <pbirnie@marketingadept.com>
2013-10-23 23:00:17 -0400

    Lazy-load-2.

---
+++ techniques/lazy-load-2.php
@@ -0,0 +1,27 @@
+<?php 
+	$pageClass = "lazy-load-2";
+	$nextURL = "";
+	$prevURL = "solution/lazy-load-1";
+	include('../inc/header.php');
+?>
+	<h1>Lazy Loading Part 2 - Use Javascript to load JS elements &amp; Related, Stylesheets</h1>
+
+	<h2>What does it do?</h2>
+	<p>Load Javascript, CSS and Markup only after all elements are loaded - and only at particular screen sizes.</p>
+
+	<h2>What does this address?</h2>
+	<ul>
+		<li>Reduce Latency</li>
+		<li>Reduce Bandwidth</li>
+		<li>Making Page Appear to Load Faster</li>
+	</ul>
+
+	<h2>Implementation</h2>
+	<ul>
+		<li><a href="https://github.com/sparkbox/mediaCheck" title="MediaCheck Plugin">MediaCheck Plugin</a></li>
+		<li><a href="<?php echo $root; ?>/techniques/jcarousel-example">jCarousel</a> as an example</li>
+	</ul>
+
+	<h2>Testing</h2>
+	<p>Analyze HAR file/Waterfall Chart for bandwidth usage, number of files loaded, and point at which DOM is loaded</p>
+<?php include('../inc/footer.php'); ?>
