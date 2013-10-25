9146310671ca9b3db98e0811c94af72bd16bf6d3
Phil Birnie <pbirnie@marketingadept.com>
2013-10-25 06:57:53 -0400

    Draft site complete.

--- demo/demo-t.php
+++ demo/demo-t.php
@@ -2,35 +2,23 @@
 	$pageClass = "demo";
 	$nextURL = "";
 	$prevURL = "";
-	include('../inc/concatenate-header.php');
+	include('../inc/header-lltest.php');
 ?>
-	<h1>Concatenation Demo</h1>
+	<h1>Lazy Loading Images</h1>
 
 	<h2>Before</h2>
 
-	<p>Before we concatenate everything, we have 19 requests.</p>
+	<p>The "mobile first" philosophy leads us to start with all styles in a base stylesheet and then override these styles with media queries.  This results in loading one extra image</p>
 
-	<h2>jCarousel Example</h2>
-	<div id="jcarousel-container">
-			<div class="jcarousel">
-			<ul>
-			<li><img src="<?php echo $root; ?>/img/jcarousel/img1.jpg"  alt=""></li>
-			<li><img src="<?php echo $root; ?>/img/jcarousel/img2.jpg"  alt=""></li>
-			<li><img src="<?php echo $root; ?>/img/jcarousel/img3.jpg"  alt=""></li>
-			<li><img src="<?php echo $root; ?>/img/jcarousel/img4.jpg"  alt=""></li>
-			<li><img src="<?php echo $root; ?>/img/jcarousel/img5.jpg"  alt=""></li>
-			<li><img class="last-image" src="<?php echo $root; ?>/img/jcarousel/img6.jpg"  alt=""></li>
-			</ul>
-			</div>
-			<p class="photo-credits">
-			Photos by <a href="http://www.mw-fotografie.de">Marc Wiegelmann</a>
-			</p>
-			<a href="#" class="jcarousel-control-prev">&lsaquo;</a>
-			<a href="#" class="jcarousel-control-next">&rsaquo;</a>
-			<p class="jcarousel-pagination">
-			</p>
-			</div>
+	<h2>After</h2>
+
+	<p>By putting <strong>all</strong> background images that will be swapped into media queries, we reduce 
+	our number of requests and bandwidth on some mobile devices. <strong>Desktop browsers are generally smarter!</strong></p>
+
+	<div id="image-holder">
+	<!-- Div with Background Image -->
 	</div>
 
 
-<?php include('../inc/concatenate-footer.php'); ?>
+
+<?php include('../inc/footer.php'); ?>
